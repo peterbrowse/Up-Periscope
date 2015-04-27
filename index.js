676687bad3988bc4f8b52e461998feb94a75deb3
Peter Browse <peter@peterbrowse.com>
2015-04-27 16:45:52 +0100

    Adding in express

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var Twitter = require('twitter');
+var Twitter = require('twitter'),
+	express = require('express'),
+	app 	= express();
  
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -7,12 +9,26 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-client.stream('statuses/filter', {track: 'LIVE on #Periscope'}, function(stream) {
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+	response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+	console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+client.stream('statuses/filter', {track: 'twitter'},  function(stream){
   stream.on('data', function(tweet) {
     console.log(tweet.text);
   });
- 
+
   stream.on('error', function(error) {
-    throw error;
+    console.log(error);
   });
 });
+
+console.log(process.env);
